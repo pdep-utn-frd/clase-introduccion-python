32454e82aaf2d6253a4a1e5175bab961a69e9175
Juan Bono <juanbono94@gmail.com>
2021-08-13 00:47:53 -0300

    agrego ejercicio y tests

---
+++ ejercicio.py
@@ -0,0 +1,100 @@
+# Cosas a tener en cuenta:
+# override de metodos - no hay.
+# objetos literales como los object de Wollok - no existen.
+
+class Pizza():
+    precio_por_tamanio = {
+        "grande": 100,
+        "mediana": 80,
+        "chica": 50
+    }
+
+    def __init__(self, tamanio):
+        self.tamanio = tamanio
+
+    def precio_base(self):
+        """
+        Calcula el precio base de una pizza
+        utilizando su tamanio.
+        """
+        return self.precio_por_tamanio[self.tamanio]
+
+    def precio_tipo(self):
+        pass
+
+    def precio(self):
+        return self.precio_base() + self.precio_tipo()
+
+
+class Muzzarella(Pizza):
+    def precio_tipo(self):
+        return 500
+
+
+class Calabresa(Pizza):
+    def precio_tipo(self):
+        if self.tamanio in ["mediana", "chica"]:
+            return 600 + self.precio_base()
+        elif self.tamanio == "grande":
+            return 700 + self.precio_base()
+
+
+class Napolitana(Pizza):
+    def precio_tipo(self):
+        return len(self.tamanio) * 100
+
+
+class CantidadInvalida(Exception):
+    def __init__(self, cantidad):
+        mensaje = f"Se ingreso una cantidad invalida ({cantidad}) de medallones."
+        # llamo al constructor de la clase Exception que toma un mensaje como
+        # parametro.
+        super().__init__(mensaje)
+
+
+class Hamburguesa():
+    def __init__(self, medallones):
+        if 4 >= medallones >= 1:
+            self._medallones = medallones
+        else:
+            raise CantidadInvalida(cantidad=medallones)
+
+    @property  # getter
+    def medallones(self):
+        return self._medallones
+
+    @medallones.setter
+    def medallones(self, medallones):
+        if 4 >= medallones >= 1:
+            self._medallones = medallones
+        else:
+            raise CantidadInvalida(cantidad=medallones)
+
+    def precio_base(self):
+        return 120
+
+    def precio(self):
+        return self.precio_base() + 200 * self.medallones
+
+
+class Flan():
+    def __init__(self, tiene_dulce_de_leche=False):
+        self.tiene_dulce_de_leche = tiene_dulce_de_leche
+
+    def precio(self):
+        if self.tiene_dulce_de_leche:
+            return 300
+        else:
+            return 280
+
+
+class Pedido():
+    def __init__(self, comidas=[]):
+        self.comidas = comidas
+
+    def agregar_comida(self, comida):
+        self.comidas.append(comida)
+
+    def precio(self):
+        precios = map(lambda c: c.precio(), self.comidas)
+        return sum(precios) * 1.15
---
+++ test.py
@@ -0,0 +1,56 @@
+# pytest
+# nosetest
+import unittest
+from ejercicio import *
+
+class EjercicioTest(unittest.TestCase):
+	def test_precio_base_grande_de_muzza_es_100(self):
+		grande_de_muzza = Muzzarella("grande")
+		self.assertEqual(grande_de_muzza.precio_base(), 100)
+
+	def test_precio_total_grande_de_muzza_es_600(self):
+		"""
+		El precio total de una grande de muzza debe ser
+		600 debido a que su precio base es 100 y las pizzas
+		de muzza cuestan 500 independientemente de su tamanio.
+		"""
+		grande_de_muzza = Muzzarella("grande")
+		precio_base = grande_de_muzza.precio_base()
+		precio_muzza = grande_de_muzza.precio_tipo()
+		self.assertEqual(grande_de_muzza.precio(), precio_muzza + precio_base)
+
+	def test_precio_napolitana_chica(self):
+		napo_chica = Napolitana("chica")
+		precio_base = napo_chica.precio_base()
+		self.assertEqual(50, precio_base)
+		precio_tipo = napo_chica.precio_tipo()
+		self.assertEqual(500, precio_tipo)
+		self.assertEqual(precio_tipo + precio_base, napo_chica.precio())
+
+	# Dato random - convenciones de nombres (de metodos/variables):
+	# testNosePuedeCrearHamburguesasInvalidas(self) <- camel case (usado java)
+	# En Lisp se usa kebab-case: test-no-se-pueden-crear-hamburguesas-invalidas
+	# En Python se usa snake case
+	# Mas info en https://en.wikipedia.org/wiki/Naming_convention_(programming)
+	def test_no_se_pueden_crear_hamburguesas_invalidas(self):
+		self.assertRaises(CantidadInvalida, Hamburguesa, 10)
+
+	def test_precio_del_flan(self):
+		flan_sin_ddl = Flan(tiene_dulce_de_leche=False)
+		self.assertEqual(280, flan_sin_ddl.precio())
+		flan_con_ddl = Flan(tiene_dulce_de_leche=True)
+		self.assertEqual(300, flan_con_ddl.precio())
+
+	def test_pedido_vacio_cuesta_0(self):
+		pedido_vacio = Pedido()
+		self.assertEqual(0, pedido_vacio.precio())
+
+	def test_pedido_con_muzza_y_flan_cuesta_900(self):
+		muzza_grande = Muzzarella(tamanio="grande")
+		flan_con_ddl = Flan(tiene_dulce_de_leche=True)
+		pedido = Pedido(comidas=[muzza_grande, flan_con_ddl])
+		sub_total = muzza_grande.precio() + flan_con_ddl.precio()
+		self.assertEqual(sub_total * 1.15, pedido.precio())
+
+# corro los tests
+unittest.main()
